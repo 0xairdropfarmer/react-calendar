e3997f953aedef9c96d358a1d7b14d3ef7abdc85
krissanawat <krissanawat101@gmail.com>
2018-12-10 07:39:04 +0700

    include calendar component
    first include calendar component to index.js

---
+++ src/components/calendar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Calendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Calendar</h2>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+import Calendar from "./components/calendar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Calendar />
     </div>
   );
 }
