ed0a49239d5912cb6490fa6b873e8b3c99dfcfda
krissanawat <krissanawat101@gmail.com>
2018-12-10 09:27:44 +0700

    include datetime.tail and display dayweekshortname

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,10 +1,25 @@
 import React from "react";
-
+import moment from "moment";
 export default class Calendar extends React.Component {
+  weekdayshort = moment.weekdaysShort();
+
   render() {
+    let weekdayshortname = this.weekdayshort.map(day => {
+      return (
+        <th key={day} className="week-day">
+          {day}
+        </th>
+      );
+    });
     return (
-      <div>
-        <h2>Calendar</h2>
+      <div className="tail-datetime-calendar">
+        <div className="calendar-date">
+          <table className="calendar-day">
+            <thead>
+              <tr>{weekdayshortname}</tr>
+            </thead>
+          </table>
+        </div>
       </div>
     );
   }
